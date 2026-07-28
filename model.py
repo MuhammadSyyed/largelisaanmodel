88e5d4fe7d03dedd483bdb4ec5a43a9656802d78
Imran Abbas <imranabbas@Imrans-Mac-mini.local>
2026-07-28 14:53:17 +0500

    Adds configs in models.py

--- model.py
+++ model.py
@@ -1,12 +1,27 @@
 from ollama import chat
 from ollama import ChatResponse
 
-response: ChatResponse = chat(model='qwen3.5:2b', messages=[
-  {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
-])
-print(response['message']['content'])
+response: ChatResponse = chat(
+    model="qwen3.5:2b",
+    think=False,
+    options={"temperature": 0.4, "max_tokens": 200},
+    messages=[
+        {
+            "role": "system",
+            "content": """You are a helpful AI assistant.
+                    Rules:
+                        - Answer only using the provided context.
+                        - If the answer is not in the context, say "I don't know."
+                        - Be concise and factual.
+                        - Respond in max 1 or 2 sentences
+                    """,
+        },
+        {
+            "role": "user",
+            "content": "Why is the sky blue?",
+        },
+    ],
+)
+
 
 print(response.message.content)
