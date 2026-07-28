56a49d1d286ad4e6c6a27cfe6844e995448b4616
Imran Abbas <imranabbas@Imrans-Mac-mini.local>
2026-07-28 14:47:50 +0500

    Adds ollama model wrapper

---
+++ wrapper.py
@@ -0,0 +1,52 @@
+from ollama import AsyncClient
+
+
+class OllamaClient:
+
+    def __init__(self):
+        self.client = AsyncClient(host="http://localhost:11434")
+
+    async def chat(self, prompt: str):
+
+        response = await self.client.chat(
+            model="qwen3.5:2b",
+            think=False,
+            options={"temperature": 0.2, "max_tokens": 200},
+            messages=[
+                {
+                    "role": "system",
+                    "content": """You are a helpful AI assistant.
+                    Rules:
+                        - Answer only using the provided context.
+                        - If the answer is not in the context, say "I don't know."
+                        - Be concise and factual.
+                        - Respond in max 1 or 2 sentences
+                    """,
+                },
+                {"role": "user", "content": prompt},
+            ],
+        )
+
+        return response['message']['content']
+
+    async def embed(self, text: str):
+
+        response = await self.client.embed(model="nomic-embed-text", input=text)
+
+        return response["embeddings"][0]
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    async def main():
+
+        client = OllamaClient()
+
+        # chat_response = await client.chat("Hello, why sky appears blue?")
+        # print("Chat Response:", chat_response)
+
+        embed_response = await client.embed("queen")
+        print("Embedding Response:", embed_response)
+
+    asyncio.run(main())
